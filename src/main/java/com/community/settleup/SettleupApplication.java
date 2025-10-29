b04fcab45b74058dfbbe75f841f730bb5fd3fbf4
Manav Gupta <37526340+manav98@users.noreply.github.com>
2025-10-29 09:11:50 +0530

    Initial commit

---
+++ src/main/java/com/community/settleup/SettleupApplication.java
@@ -0,0 +1,13 @@
+package com.community.settleup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettleupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/community/settleup/SettleupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.settleup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettleupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
